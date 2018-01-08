7c96b8f84e8d0a9bb554899faced82b981e4b518
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2018-01-09 00:31:44 +0100

    Provided JWT SECRET for Production

--- server/config/config.js
+++ server/config/config.js
@@ -8,4 +8,11 @@ if (env === 'development' || env === 'test') {
     Object.keys(envConfig).forEach(key => {
         process.env[key] = envConfig[key];
     })
+} else {
+    const config = require('./config.json');
+    const envConfig = config[env];
+
+    Object.keys(envConfig).forEach(key => {
+        process.env[key] = envConfig[key]
+    });
 }
