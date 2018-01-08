f8c3a57206c3bf34935472e0581b24cbc5fa2563
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2018-01-08 21:04:50 +0100

    Rerouting endpoints for Production

--- client/scripts/signup.js
+++ client/scripts/signup.js
@@ -6,7 +6,8 @@ const loader = document.getElementById('loader');
 const notifyBox = document.getElementById('notify-box');
 const cancelBtn = document.getElementById('cancel-btn');
 // const url = 'http://localhost:3000/';
-const url = 'https://pacific-stream-32452.herokuapp.com:30863/';
+// const url = 'https://pacific-stream-32452.herokuapp.com:30863/';
+const url = 'https://localhost:30863';
 
 loader.style.display = 'none';
 notifyBox.style.display = 'none';
